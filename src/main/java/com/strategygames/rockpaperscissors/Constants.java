75d07c07d2275ef99a0a4976fc6d15dd723a7c47
Rohith Uppala <rohith.uppala@booking.com>
2018-10-13 20:01:13 +0200

    Removed the public method for static variables

--- src/main/java/com/strategygames/rockpaperscissors/Constants.java
+++ src/main/java/com/strategygames/rockpaperscissors/Constants.java
@@ -29,8 +29,8 @@ public class Constants {
         OptionsMap.put(SCISSORS, "Scissors");
     }
 
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_CYAN = "\u001B[36m";
+    static final String ANSI_GREEN = "\u001B[32m";
+    static final String ANSI_BLUE = "\u001B[34m";
+    static final String ANSI_RESET = "\u001B[0m";
+    static final String ANSI_CYAN = "\u001B[36m";
 }
